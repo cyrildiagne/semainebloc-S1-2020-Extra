17ef0bec4d1c2c671c199878524e4e5ed77a488d
OhJorgeReis <43137845+OhJorgeReis@users.noreply.github.com>
2020-12-07 15:03:56 +0100

    changes css

--- intro/Jorge/simple/content.js
+++ intro/Jorge/simple/content.js
@@ -2,5 +2,5 @@ console.log("hello from  fromcontent.js")
 
 const imgs = document.body.getElementsByTagName('img')
 for (const img of imgs) {
-    img.style.transform = 'rotate(45deg)';
+    img.classList.add("custom-img")
 }
