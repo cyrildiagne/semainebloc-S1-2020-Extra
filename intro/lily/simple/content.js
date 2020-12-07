623f8716abca14f1ac3cfb2a736addcde50f6cfd
lilyroseh <47317192+lilyroseh@users.noreply.github.com>
2020-12-07 14:59:44 +0100

    update

--- intro/lily/simple/content.js
+++ intro/lily/simple/content.js
@@ -1,10 +1,16 @@
 const ps = document.body.querySelectorAll("p");
 //const img = document.body.querySelector
+const imgs = document.body.getElementsByTagName("img");
 
+
+for (const img of imgs) {
+    img.style.transform = "rotate(45deg)";
+
+}
 
 
 for (const p of ps){
-    p.style.transform = rotate(90);
+    p.style.transform = "rotate(90deg)";
 } 
 
 
