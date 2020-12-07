459105dfaf39fdec8460269a37e120fe9060e892
OhJorgeReis <43137845+OhJorgeReis@users.noreply.github.com>
2020-12-07 14:28:35 +0100

    NOUVEAU DOSSIER

---
+++ Jorge/simple/background.js
@@ -0,0 +1,11 @@
+function onClicked(tab) {
+    chrome.windows.create({
+        url: chrome.runtime.getURL("popup.html"),
+        type: "popup",
+        focused:true,
+    });
+}
+
+chrome.browserAction.onClicked.addListener(onClicked)
+
+console.log('hello world')
