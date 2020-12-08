3907e6773cda050d41fcd432867db08579251948
Mélanie <melanief0968@yahoo.fr>
2020-12-08 10:54:59 +0100

    add changes

--- intro/melanie/content.js
+++ intro/melanie/content.js
@@ -2,7 +2,7 @@
 const imgs = document.body.getElementsByTagName("img");
 
 for (const img of imgs) {
-    img.style.transform = "rotate(" + Math.random(1)*100  + "deg)";
-    img.classList.add("rotate");
+    // img.style.transform = "rotate(" + Math.random(1)*100  + "deg)";
+    img.classList.add('custom-img');
     console.log('sisi');
 }
