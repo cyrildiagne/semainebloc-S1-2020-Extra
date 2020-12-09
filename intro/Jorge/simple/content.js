9cff15d02cf5379a38c13e1b8c5eaf907539d4a6
OhJorgeReis <43137845+OhJorgeReis@users.noreply.github.com>
2020-12-09 14:39:02 +0100

    update

--- intro/Jorge/simple/content.js
+++ intro/Jorge/simple/content.js
@@ -1,25 +1,24 @@
+
 console.log("hello from  fromcontent.js")
 
-const imgs = document.body.getElementsByTagName('img')
-for (const img of imgs) {
-    img.classList.add("custom-img")
-}
-
-        var requestOptions = {
+function getAirQuality({city, state, country}){
+  var requestOptions = {
             method: 'GET',
             redirect: 'follow'
           };
-      
-        fetch(
-          'https://api.airvisual.com/v2/city?city=Lisbon&state=Lisbon&country=Portugal&key=7021151e-c3e9-4030-8ea7-c0316bd18666', requestOptions)
+
+           fetch(
+          'https://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key=7021151e-c3e9-4030-8ea7-c0316bd18666', requestOptions)
       .then(response => response.json())
       .then(result => console.log(result))
       .catch(error => console.log('error', error));
-    
-    function run(){
-      const city = 'Sathon';
-      const state = 'Bangkok';
-      const country = 'Thailand';
-    
-      getAirQuality({city, state, country});
-    }
+            }
+
+function run(){
+ const city = 'Sathon';
+const state = 'Bangkok';
+const country = 'Thailand';
+
+getAirQuality({city, state, country});
+}
+run();
