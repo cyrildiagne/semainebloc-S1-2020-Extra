b8338f9a13c00b434f04beadb77a30208c02228b
Martial Grin <martial.grin@gmail.com>
2020-12-09 14:31:00 +0100

    s

--- intro/martial/pixi__tuesday-morning/src/content.js
+++ intro/martial/pixi__tuesday-morning/src/content.js
@@ -1,6 +1,48 @@
-const circle = new PIXI.Graphics()
-    .beginFill(0xff00ff)
-    .drawCircle(100, 100, 30)
-    .endFill()
+let x = 0;
+let y = 0;
 
-pixi.stage.addChild(circle)
+class Content {
+  constructor() {
+    this.setup();
+  }
+
+  setup() {
+    this.startListeners();
+    this.circle = new PIXI.Graphics()
+      .beginFill(0xff00ff)
+      .drawCircle(x, y, 30)
+      .endFill();
+    pixi.stage.addChild(this.circle);
+    pixi.stage.interactive = true;
+    this.draw();
+  }
+
+  startListeners() {
+    // this.mouseDownHandler = this.onMouseDown.bind(this);
+    // this.mouseUpHandler = this.onMouseUp.bind(this);
+    this.mouseMoveHandler = this.onMouseMove.bind(this);
+
+    // document.addEventListener("mousedown", this.mouseDownHandler);
+    // document.addEventListener("mouseup", this.mouseUpHandler);
+    document.addEventListener("mousemove", this.mouseMoveHandler);
+  }
+
+  draw() {
+    requestAnimationFrame(this.draw.bind(this));
+  }
+
+  onMouseDown(e) {
+    this.circle.beginFill();
+  }
+  onMouseMove(e) {
+    this.circle.position.x = e.clientX;
+    this.circle.position.y = e.clientY;
+  }
+}
+
+window.onload = () => {
+  new Content();
+};
+
+// this.keyDownHandler = this.onKeyDown.bind(this);
+// this.keyUpHandler = this.onKeyUp.bind(this);
