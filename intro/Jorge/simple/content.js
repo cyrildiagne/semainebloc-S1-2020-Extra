529117aaedee61e6bc02d6ee70cc0a4cdd0c9108
OhJorgeReis <43137845+OhJorgeReis@users.noreply.github.com>
2020-12-09 03:52:50 +0100

    ADD api

--- intro/Jorge/simple/content.js
+++ intro/Jorge/simple/content.js
@@ -4,3 +4,22 @@ const imgs = document.body.getElementsByTagName('img')
 for (const img of imgs) {
     img.classList.add("custom-img")
 }
+
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+      
+        fetch(
+          'https://api.airvisual.com/v2/city?city=Lisbon&state=Lisbon&country=Portugal&key=7021151e-c3e9-4030-8ea7-c0316bd18666', requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    
+    function run(){
+      const city = 'Sathon';
+      const state = 'Bangkok';
+      const country = 'Thailand';
+    
+      getAirQuality({city, state, country});
+    }
