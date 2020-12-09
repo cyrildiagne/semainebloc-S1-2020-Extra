90a60f3b138906e6288ea3e5f4b7bba1d603b835
OhJorgeReis <43137845+OhJorgeReis@users.noreply.github.com>
2020-12-09 15:05:09 +0100

    Correction

--- intro/Jorge/simple/content.js
+++ intro/Jorge/simple/content.js
@@ -1,10 +1,4 @@
 
-<<<<<<< HEAD
-const imgs = document.body.getElementsByTagName('img')
-for (const img of imgs) {
-    img.classList.add("custom-img")
-}
-=======
 console.log("hello from  fromcontent.js")
 const API = 'https://api.airvisual.com/v2/city?city=';
 const KEY = '7021151e-c3e9-4030-8ea7-c0316bd18666';
@@ -32,4 +26,4 @@ function run(){
 getAirQuality({city, state, country});
 }
 run();
->>>>>>> 7d7584bce02b4eae2889165fbcdef896921e305c
+
