1158e5bad5a73f7ae3baf2111440dbe0b5a459ae
Antoine <antoinecontreras98@gmail.com>
2020-12-09 13:27:53 +0100

    v4

--- projects/antoine/linkedin_broken/content.js
+++ projects/antoine/linkedin_broken/content.js
@@ -1,11 +1,13 @@
 // DeepAI API KEY
+// console.log("linkedin ok")
 const API_KEY = 'dd7c38c7-9a46-48b3-8b78-0696a284771f';
 deepai.setApiKey(API_KEY);
-
 async function run() {
+  // console.log("linkedin ok")
   let ps = document.body.querySelectorAll('p');
-
+  
   for (const p of ps) {
+    // console.log(ps);
     if (p.innerText == '') {
       continue;
     }
@@ -15,11 +17,14 @@ async function run() {
         text: p.innerText,
       });
       console.log(resp);
+     
       // h.dataset['rhyme'] = rhyme;
     } catch (e) {
       console.log(e);
     }
+   
   }
+  // return
 }
 
 // Listen for background script message when the button has been clicked.
