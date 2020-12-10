92e72041b8737519f358ae88e5da834d76793831
Cyril Diagne <diagne.cyril@gmail.com>
2020-12-10 10:58:06 +0100

    fix bug

--- projects/salome/web-wanderer/src/inject/inject.js
+++ projects/salome/web-wanderer/src/inject/inject.js
@@ -12,23 +12,42 @@ canvas.classList.add('canvas');
 const ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
 
+const container = document.createElement('div');
+container.classList.add('container');
+document.body.appendChild(container);
+
 function drawCanvas(history) {
-  for (const page of history) {
-    console.log(page);
-    ctx.beginPath();
-    ctx.fillStyle = 'red';
-    ctx.ellipse(page.coords.x, page.coords.y, 50, 50, 0, 0, Math.PI * 2);
-    ctx.fill();
+  // Add link
+  for (const link of history) {
+    console.log(link);
+    const a = document.createElement('a');
+    a.innerText = link.text;
+    a.href = link.href;
+    a.classList.add('link')
+    a.style.left = Math.floor(link.coords.x) + 'px';
+    a.style.top = Math.floor(link.coords.y) + 'px';
+    container.appendChild(a);
   }
+
+  // Draw lines
+  ctx.beginPath();
+  ctx.strokeStyle = 'red';
+  ctx.lineWidth = 5;
+  ctx.moveTo(history[0].coords.x + 50, history[0].coords.y + 50);
+  for (const link of history) {
+    ctx.lineTo(link.coords.x + 50, link.coords.y + 50);
+  }
+  ctx.stroke();
 }
 
 async function load() {
   await waitForDocumentReady();
-  chrome.runtime.sendMessage('getHistory', (resp, test) => {
+  chrome.runtime.sendMessage({ action: 'getHistory' }, resp) => {
     console.log('history is:', resp, test);
+    console.log(chrome.runtime.lastError);
     if (resp) {
       console.log('history draw canvas');
-      drawCanvas(resp);
+      drawCanvas(resp.links);
     }
   });
 }
