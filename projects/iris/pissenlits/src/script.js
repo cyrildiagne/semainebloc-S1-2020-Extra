e1ae5fddcfe43084cc243e9a7adb3f0ba6b99ae4
Sébastien Matos <sess22@gmail.com>
2020-12-10 12:42:23 +0100

    iris move player

--- projects/iris/pissenlits/src/script.js
+++ projects/iris/pissenlits/src/script.js
@@ -1,51 +1,47 @@
 //ajout array avec toutes mes urls
-const allMyUrls = [
-  'res/pissenlits.json',
-  'res/pissenlits-blue.json',
-  'res/little-fish.json',
-  'res/cloud-2.json',
+const PLAYERS = [];
+const ANIMATION_PATHS = [
+  "res/pissenlits.json",
+  "res/pissenlits-blue.json",
+  "res/little-fish.json",
+  "res/cloud-2.json",
+  "res/algaes.json",
 ];
-
 //
-const width = 100;
-const height = 100;
+const width = 200;
+const height = 200;
+
+const MAX_N_PLAYERS = 5;
+let P = new p5();
+
+// console.log();
 
 function addVisual(x, y) {
-  const el = document.createElement('div');
-  el.classList.add('lottie-anim');
-  el.style.left = x - width * 0.5 + 'px';
-  el.style.top = y - height * 0.5 + 'px';
-  el.style.width = width + 'px';
-  el.style.height = height + 'px';
 
-  const index = Math.floor(Math.random() * 4);
-  let url = allMyUrls[index];
+  if(MAX_N_PLAYERS <= PLAYERS.length)
+    return;
 
-  url = allMyUrls[3];
+  let animationPath = P.random(ANIMATION_PATHS);
+  let player = new Visual(animationPath, x, y);
 
-  bodymovin.loadAnimation({
-    container: el,
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    },
-    path: chrome.runtime.getURL(url),
-  });
-  document.body.append(el);
+  PLAYERS.push(player);
 }
 
-// const frequency = 5000; // in ms
-// setInterval(() => {
-//   const x = window.innerWidth * Math.random();
-//   const y = window.innerHeight * Math.random();
-//   addVisual(x, y);
-// }, frequency);
+function moveVisual(x, y) {
+  let v = createVector(x - mouseX, y - mouseY);
+  let speed = map(v.mag(), 0, 200, 5, 0);
+  v.setMag(speed);
+}
+setInterval(() => {
+  addVisual(P.random(window.innerWidth), P.random(window.innerHeight));
+}, 1000);
 
+update();
 
-addVisual(window.innerWidth/2, window.innerHeight/2);
+function update() {
+  for (let player of PLAYERS) {
+    player.avoid(P.winMouseX, P.winMouseY);
+  }
 
-// document.body.addEventListener('click', (ev) => {
-//   addVisual(ev.clientX, ev.clientY)
-// });
+  requestAnimationFrame(update);
+}
