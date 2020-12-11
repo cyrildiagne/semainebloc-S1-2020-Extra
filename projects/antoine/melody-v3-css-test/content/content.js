c8c7b3fc0abcbcf1ca43d75566a9320ec6b53647
Antoine <antoinecontreras98@gmail.com>
2020-12-11 02:10:30 +0100

    check array tone

--- projects/antoine/melody-v3-css-test/content/content.js
+++ projects/antoine/melody-v3-css-test/content/content.js
@@ -15,9 +15,9 @@ let NOTES = {
 const port = chrome.runtime.connect({ name: "tab-synth" });
 port.onMessage.addListener(function (msg) {
 
-  
 
-  if(msg.tabId !== TAB_ID)
+
+  if (msg.tabId !== TAB_ID)
     return;
 
   if (msg.action === "playNote") {
@@ -44,50 +44,50 @@ deepai.setApiKey(API_KEY);
 async function run() {
   // getPosts();
 
-  // let ps = document.body.querySelectorAll("p");
+  let ps = document.body.querySelectorAll("p");
 
-  // for (const p of ps) {
-  //   if (p.innerText == "") {
-  //     continue;
-  //   }
-  //   //GET TEXT WITH API
-  //   try {
-  //     //REDUCE TEXT API OK
-  //     // const resp = await deepai.callStandardApi('summarization', {
-  //     //   text: p.innerText,
-  //     // });
+  for (const p of ps) {
+    if (p.innerText == "") {
+      continue;
+    }
+    //   //GET TEXT WITH API
+    try {
+      //     //REDUCE TEXT API OK
+      //     // const resp = await deepai.callStandardApi('summarization', {
+      //     //   text: p.innerText,
+      //     // });
 
-  //     //SENTIMENT TEXT API OK
-  //     //REPONSE = respSen.output
-  //     let respSen = await deepai.callStandardApi("sentiment-analysis", {
-  //       //TEST LOCAL TEXT OK
-  //       // text: "Today, the One Planet Sovereign Wealth Funds has demonstrated that reinforced cooperation between global financial actors is key to accelerate the path to a net zero economy and implement the Paris Agreement. Your announcement on Task Force on Climate-related Financial Disclosures recommendations is a pivotal moment. I count on all members, sovereign wealth funds, asset managers, private equity firms to continue to use their collective power and ambition to accelerate the financing of the ecological transition and mitigate climate change.",
-  //       text: p.innerText,
-  //     });
-  //     if (respSen.output == "Negative") {
-  //       console.log("Negative FILTER");
-  //     } else if (respSen.output == "Positive") {
-  //       console.log("Positive FILTER");
-  //     } else if (respSen.output == "Neutral") {
-  //       console.log("Neutral FILTER");
-  //     } else {
-  //       console.log("OUTSIDE FILTER BUG");
-  //     }
+      //     //SENTIMENT TEXT API OK
+      //     //REPONSE = respSen.output
+      // let respSen = await deepai.callStandardApi("sentiment-analysis", {
+      //   //TEST LOCAL TEXT OK
+      //   // text: "Today, the One Planet Sovereign Wealth Funds has demonstrated that reinforced cooperation between global financial actors is key to accelerate the path to a net zero economy and implement the Paris Agreement. Your announcement on Task Force on Climate-related Financial Disclosures recommendations is a pivotal moment. I count on all members, sovereign wealth funds, asset managers, private equity firms to continue to use their collective power and ambition to accelerate the financing of the ecological transition and mitigate climate change.",
+      //   text: p.innerText,
+      // });
+      // if (respSen.output == "Negative") {
+      //   console.log("Negative FILTER");
+      // } else if (respSen.output == "Positive") {
+      //   console.log("Positive FILTER");
+      // } else if (respSen.output == "Neutral") {
+      //   console.log("Neutral FILTER");
+      // } else {
+      //   console.log("OUTSIDE FILTER BUG");
+      // }
 
-  //     // console.log(p.innerText + " ==  " + respSen.output);
-  //     //END
+      //     // console.log(p.innerText + " ==  " + respSen.output);
+      //     //END
 
-  //     //CLICK TO SET UP SOUNDSCAPE
-  //     document.documentElement.addEventListener("mousedown", () => {
-  //       if (Tone.context.state !== "running") Tone.context.resume();
-  //     });
-  //     //END SET UP
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  //   //END GET TEXT WITH API
-  // }
-  // return
+      //     //CLICK TO SET UP SOUNDSCAPE
+      //     document.documentElement.addEventListener("mousedown", () => {
+      //       if (Tone.context.state !== "running") Tone.context.resume();
+      //     });
+      //     //END SET UP
+    } catch (e) {
+      //     console.log(e);
+    }
+    //   //END GET TEXT WITH API
+  }
+  return
 }
 
 chrome.runtime.onMessage.addListener((message, sender) => {
@@ -96,7 +96,28 @@ chrome.runtime.onMessage.addListener((message, sender) => {
   }
 });
 
-async function analyseText() {
+async function analyseText(e) {
+  // console.log(e) 
+  let respSen = await deepai.callStandardApi("sentiment-analysis", {
+    text: e,
+
+  });
+  console.log(respSen.output.length);
+  for (let index = 0; index < respSen.output.length-1; index++) {
+    const elMoyenne = respSen.output[index];
+    if ( elMoyenne == "Negative") {
+      console.log("Negative FILTER");
+    } else if (elMoyenne == "Positive") {
+      console.log("Positive FILTER");
+    } else if (elMoyenne == "Neutral") {
+      console.log("Neutral FILTER");
+    } else {
+      console.log("OUTSIDE FILTER BUG");
+    }
+
+  }
+
+
   return "Positive";
 }
 
@@ -105,9 +126,8 @@ function analyseNewPosts() {
   posts.forEach(async (post) => {
     if (isNewTextPost(post)) {
       let description = post.querySelector('[class*="commentary"]');
-
       setListener(post);
-      let output = await analyseText(post);
+      let output = await analyseText(description.textContent);
       let note = NOTES[output];
       post.dataset.tonenote = note;
       // console.log(description, description.textContent);
@@ -118,7 +138,7 @@ function analyseNewPosts() {
 function setListener(post) {
   post.addEventListener("mouseenter", () => {
     // post.classList.toggle("tone--active");
-    $(post).toggleClass( "tone--active" );
+    $(post).toggleClass("tone--active");
     post.classList.toggle("artdeco-card");
     post.classList.toggle("full-height");
     let activePosts = getAllPlayingPosts();
